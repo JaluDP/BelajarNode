959748d8179fe45593e8a23b5ddcc52bf4497ad6
JaluDP <jdpscript@gmail.com>
2026-06-04 15:46:45 +0700

    latihan 1 Menjalankan file node

---
+++ index.js
@@ -0,0 +1,8 @@
+// const nama = "jalu";
+// const cetakNama = (nama) => `Hi, nama saya ${nama}`;
+// console.log(cetakNama('Jalu dwi Purwandana'));
+const cetakNama = require('./coba');
+
+// console.log("Hello Jalu !");
+
+console.log(cetakNama('Jalu Dwi P'));
