b80817279e268f36ec44cb951315e0c090129ea0
JaluDP <jdpscript@gmail.com>
2026-06-06 06:01:57 +0700

    latihan 4 lanjutan menggunakan NPM

--- latihan 4/app.js
+++ latihan 4/app.js
@@ -1,1 +1,10 @@
-console.log('Hello World!');
+const validator = require('validator');
+const chalk = require('chalk');
+// console.log(validator.isEmail('jadwip@gmail.com'));
+// console.log(validator.isMobilePhone('08123456789','id-ID'));
+// console.log(validator.isNumeric('0812345678'));
+
+// console.log(chalk.italic.bgBlueBright.black('Hello World!'));
+const nama = 'jalu';
+const pesan = chalk`Lorem, ipsum dolor {bgGreen.black.bold sit amet} consectetur {bgMagenta.italic.white adipisicing} elit. Distinctio, similique. nama saya : ${nama}`;
+console.log(pesan);
