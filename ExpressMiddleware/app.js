ca696dd9d600f190eeedc6853930b55b17b5c188
JaluDP <jdpscript@gmail.com>
2026-06-10 21:25:09 +0700

    Belajar Express Middleware

--- ExpressMiddleware/app.js
+++ ExpressMiddleware/app.js
@@ -1,12 +1,25 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const morgan = require('morgan');
 const app = express();
 const port = 3000;
 
 // gunakan ejs
 app.set('view engine', 'ejs');
 
+//Thisrd-party Middleware
 app.use(expressLayouts);
+app.use(morgan('dev'));
+
+//Application level middleware
+app.use((req, res, next) => {
+    console.log('Time: ',Date.now());
+    next();
+});
+
+//built-in middleware
+app.use(express.static('public'));
+
 
 app.get('/', (req, res) => {
     // res.sendFile('./index.html', {root: __dirname });
@@ -36,6 +49,7 @@ app.get('/about', (req, res) => {
         layout: 'layouts/main-layout',
         title: 'Halaman About'});
 });
+
 app.get('/contact', (req, res) => {
     res.render('contact', {
         layout: 'layouts/main-layout',
@@ -46,7 +60,7 @@ app.get('/product/:id', (req, res) => {
     res.send(`Product ID : ${req.params.id} <br> Category ID : ${req.query.category}`);
 });
 
-app.use('/', (req, res) => {
+app.use((req, res) => {
     res.status(404);
     res.send('<h1>404</h1>');
 });
