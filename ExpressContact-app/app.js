6b71db7328ee38694fbf76261aa7ab0ac6008cb5
JaluDP <jdpscript@gmail.com>
2026-06-10 22:39:33 +0700

    Express Contact App Bagian 1

--- ExpressContact-app/app.js
+++ ExpressContact-app/app.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
+const { loadContact, findContact } = require('./utils/contacts.js');
+
 const app = express();
 const port = 3000;
 
@@ -41,9 +43,21 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/contact', (req, res) => {
+    const contacts = loadContact();
     res.render('contact', {
         layout: 'layouts/main-layout',
-        title: 'Halaman Contact'});
+        title: 'Halaman Contact',
+        contacts,
+    });
+});
+
+app.get('/contact/:nama', (req, res) => {
+    const contact = findContact(req.params.nama);
+    res.render('detail', {
+        layout: 'layouts/main-layout',
+        title: 'Halaman Detail Contact',
+        contact,
+    });
 });
 
 app.use((req, res) => {
